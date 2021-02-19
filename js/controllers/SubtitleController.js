ee263dd7106b138d9952fce265c286941280dd31
Andrés <usuario616@gmail.com>
2021-02-19 11:41:46 +0100

    Implementado el botón para añadir anuncio

--- js/controllers/SubtitleController.js
+++ js/controllers/SubtitleController.js
@@ -10,8 +10,13 @@ export default class SubtitleController extends BaseController {
 
     async render() {
         const logged = await dataService.isUserLogged();
+        
         if(logged) {
-            
+            const butonAdd = document.querySelector('.buttonAdd');
+            butonAdd.addEventListener('click', (event) => {
+                window.location.href = '/new-ad.html';
+            });
+            butonAdd.classList.remove('hidden');
             const user = dataService.getUserDetails();
             const logInfo = document.querySelector('.subtitle');
             logInfo.innerHTML = logInfoView(user.username);//`<div style="font-size: 0.7rem;">Conectado como</div> ${user.username}&nbsp;<a href=""><img class="logout-icon" src="./public/images/logout.png" width="15"/></a>`;
