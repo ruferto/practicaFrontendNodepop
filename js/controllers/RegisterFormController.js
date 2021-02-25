08990d566a3f2e8e6c6257c8bbdc5a2e23b2f92d
Andrés <usuario616@gmail.com>
2021-02-25 13:08:48 +0100

    Añadida comprobación de confirmación de contraseña

--- js/controllers/RegisterFormController.js
+++ js/controllers/RegisterFormController.js
@@ -33,27 +33,38 @@ export default class RegisterFormController extends BaseController {
             }
         });
 
-        // this.element.querySelectorAll('input').forEach(input => {
-        //     const button = this.element.querySelector('button');
-        //     input.addEventListener('keyup', event => { 
-        //         // si el input es OK lo marco en verde, si no, en rojo
-        //         if (input.validity.valid) {
-        //             input.classList.add('is-success');
-        //             input.classList.remove('is-danger');
-        //         } else {
-        //             input.classList.remove('is-success');
-        //             input.classList.add('is-danger');
-        //         }
+        this.element.querySelectorAll('input[type="password"]').forEach(input => {
+            input.addEventListener('keyup', event => { 
+                const passInput = this.element.elements['login-password'];
+                const passConfirmInput = this.element.elements['login-password2'];
+                if (passInput.value !== passConfirmInput.value) {
+                    passInput.setCustomValidity('Las contraseñas no coinciden'); // marco el input como erróneo
+                    passConfirmInput.setCustomValidity('Las contraseñas no coinciden'); // marco el input como erróneo
+                    passInput.validationMessage = passConfirmInput.validationMessage = 'Las contraseñas no coinciden'
+                } else {
+                    passInput.setCustomValidity(''); // el input está ok
+                    passConfirmInput.setCustomValidity(''); // el input está ok
+                }
+                this.checkInputErrors();
+            });
+        });
+    }
 
-        //         // valido si todo el formulario es OK para habilitar o deshabilitar el botón
-        //         if (this.element.checkValidity()) {
-        //             button.removeAttribute('disabled');
-        //             // button.setAttribute('disabled', false); // esto también valdría
-        //         } else {
-        //             button.setAttribute('disabled', true);
-        //         }
-        //     });
-        // });
+    checkInputErrors() {
+        this.element.querySelectorAll('input').forEach(input => {
+            const button = this.element.querySelector('button');
+            if (input.validity.valid) {
+                button.disabled=true;
+            } else {
+                button.disabled=false;
+            }
+
+            if (this.element.checkValidity()) {
+                button.removeAttribute('disabled');
+            } else {
+                button.setAttribute('disabled', true);
+            }
+        });
     }
 
 }
